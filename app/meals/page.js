f1428b05c388858d5b6523f31def21c06bbba8ba
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2023-12-22 19:16:15 +0100

    storing server actions in separate file

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,22 @@
-import Link from "next/link"
-import classes from "./page.module.css"
-import MealsGrid from "@/components/meals/meals-grid"
+import { Suspense } from 'react';
+import Link from 'next/link';
+
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created{" "}
+          Delicious meals, created{' '}
           <span className={classes.highlight}>by you</span>
         </h1>
         <p>
@@ -18,8 +27,10 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
-  )
+  );
 }
